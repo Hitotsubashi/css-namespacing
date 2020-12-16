22a155aefe4057da4cde834b4ec34ed39736f718
Hitotsubashi <m13534808886@163.com>
2020-12-16 20:23:19 +0800

    feat(src): complete attributeselector-handler

--- src/attributeseletor-handler.js
+++ src/attributeseletor-handler.js
@@ -1,5 +1,24 @@
 function AttributeselectorHandler() {
   this.recordIndex = 0;
   this.record = {};
-  this.ATTR_SELECTOR_REG = new RegExp(/(\[[^=]+=\])/);
+  this.ATTR_SELECTOR_REG = new RegExp(/(\s*\[[^=]+=("[^"]*")?('[^']*')?([^\]]*)?\]\s*)/, 'gm');
+  this.ATTR_MARK_REG = new RegExp(`(${AttributeselectorHandler.ATTR_MARK})`, 'gm');
+  this.collectAttr = (content) => content.replace(this.ATTR_SELECTOR_REG, (attr) => {
+    const mark = `^${this.recordIndex}`;
+    this.recordIndex += 1;
+    this.record[mark] = attr;
+    return mark;
+  });
+  this.resetAttr = (content, prefix) => content.replace(this.ATTR_MARK_REG,
+    (mark) => this.record[mark].replace(
+      /\[(class.?=)['"]?([^'"\]]*?)['"]?\]/gm,
+      (object, left, right) => {
+        let modifiedRight = right;
+        modifiedRight = modifiedRight.split(/\s+/).map((el) => `${prefix}${el}`).join(' ');
+        return `[${left}"${modifiedRight}"]`;
+      },
+    ));
 }
+
+AttributeselectorHandler.ATTR_MARK = '\\^\\d+';
+module.exports = AttributeselectorHandler;
---
+++ tests/file/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const namespacing = require('../../src/index.js');
+
+const originDirName = 'origin';
+const modifiedDirName = 'modified';
+function getFileList() {
+  return fs.readdirSync(path.resolve(__dirname, `./${originDirName}`));
+}
+
+function readFile(filename) {
+  return new Promise((resolve, reject) => {
+    const url = path.resolve(__dirname, `./${originDirName}/${filename}`);
+    fs.readFile(url, 'utf8', (err, data) => {
+      if (err) reject(err);
+      resolve(data);
+    });
+  });
+}
+
+function writeFile(filename, data) {
+  const url = path.resolve(__dirname, `./${modifiedDirName}/${filename}`);
+  fs.appendFile(url, data, { flag: 'w' }, (err) => { if (err) throw err; });
+}
+
+const files = getFileList();
+files.forEach(async (filename) => {
+  let data = await readFile(filename);
+  data = namespacing(data);
+  writeFile(filename, data);
+});
